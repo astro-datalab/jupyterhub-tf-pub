237778a65429a41032221e511370c98886b251bf
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2023-06-08 13:07:27 -0700

    stopping recursive symlink in notebooks-latest

--- scripts/init_notebook.sh
+++ scripts/init_notebook.sh
@@ -12,7 +12,7 @@ mkdir /home/${NB_USER}/.datalab
 mkdir /home/${NB_USER}/.notebooks
 mkdir /home/${NB_USER}/jupyterhub-singleuser
 chmod 777 /home/${NB_USER}/.local
-ln -s /mnt/shared/notebooks-latest /home/${NB_USER}/notebooks-latest
+ln -shf /mnt/shared/notebooks-latest /home/${NB_USER}/notebooks-latest
 echo ${UPSTREAM_TOKEN} > /home/${NB_USER}/.datalab/id_token.${NB_USER}
 
 read -r -d  '' config<<EOF
