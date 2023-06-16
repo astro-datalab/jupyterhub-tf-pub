36c1db6967960374ba6aa010707300ffc098eacd
chaduhduh <chadd.myers3@gmail.com>
2023-06-16 14:36:24 -0700

    change target directory

--- scripts/hooks/refresh_notebooks_latest.sh
+++ scripts/hooks/refresh_notebooks_latest.sh
@@ -3,4 +3,4 @@
 ../deploy.sh \
    --name notebooks-latest \
    --source git@github.com:astro-datalab/notebooks-latest.git \
-   --target-dir /mnt/pvc/datalab/shared/notebooks-latest
+   --target-dir /usr/src/notebooks-latest
