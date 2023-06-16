c37fde80eb90a6bd785e47d352f73a0a0b17c7cd
Igor Suarez-Sola <igor.suarez-sola@noirlab.edu>
2023-06-16 15:27:04 -0700

    download and install /nbdata

--- scripts/init_shared.sh
+++ scripts/init_shared.sh
@@ -4,32 +4,41 @@
 # public repo supporting jupyterhub-tf terraform scripts
 
 PUB_REPO="jupyterhub-tf-pub"
-
-#shared directory
-TGT_DIR="/mnt/pvc/datalab/shared"
+SHARED_DIR="/mnt/pvc"
+NB_DIR="${SHARED_DIR}/datalab/shared"
+NBDATA_DIR="${SHARED_DIR}/data"
 
 # create shared dir if not present
-if [ ! -d "$TGT_DIR" ]
+if [ ! -d "$NB_DIR" ]
 then
- mkdir -p $TGT_DIR
+ mkdir -p "$NB_DIR"
 fi
+
 
 # if notebooks-latest is present get a new one
-if [ -d "${TGT_DIR}/notebooks-latest" ]
+if [ -d "${NB_DIR}/notebooks-latest" ]
 then
-  rm ${TGT_DIR}/notebooks-latest
+  rm -rf "${NB_DIR}/notebooks-latest"
 fi
 
-git clone https://github.com/astro-datalab/notebooks-latest.git ${TGT_DIR}/notebooks-latest
+git clone https://github.com/astro-datalab/notebooks-latest.git "${NB_DIR}/notebooks-latest" || exit 1
 
-# if supporting repo not present get it from github
-if [ ! -d "$PUB_REPO" ]
+if [ ! -d "${NBDATA_DIR}" ]
 then
-  git clone --depth 1  https://github.com/astro-datalab/${PUB_REPO}.git
-fi
+  mkdir "${NBDATA_DIR}"
 
-# copy what's needed to the shared directory
-cp -pr ./${PUB_REPO}/scripts ${TGT_DIR}/
+  python3.9 << EOF || exit 1
+from dl import storeClient
+ANON_TOKEN = "anonymous.0.0.anon_access"
+storeClient.get(token=ANON_TOKEN, fr="robertdemo://public/nbdata-20230616.tgz", to="./nbdata.tgz")
+EOF
+
+  if [ -f "./nbdata.tgz" ]
+  then
+    tar xf ./nbdata.tgz -C "${NBDATA_DIR}"
+  fi
+fi
 
 # clean up
-rm -rf ./${PUB_REPO}
+rm -rf ./"${PUB_REPO}"
+rm -rf ./nbdata.tgz
