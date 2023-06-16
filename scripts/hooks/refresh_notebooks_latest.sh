32626910ec5043403b9761fbb1d3e4b8f449c2ae
chaduhduh <chadd.myers3@gmail.com>
2023-06-16 13:42:41 -0700

    fixes path

--- scripts/hooks/refresh_notebooks_latest.sh
+++ scripts/hooks/refresh_notebooks_latest.sh
@@ -1,4 +1,4 @@
-../scripts/deploy.sh \
+../deploy.sh \
    --name notebooks-latest \
    --source git@github.com:astro-datalab/notebooks-latest.git \
    --target-dir /mnt/pvc/datalab/shared/notebooks-latest
